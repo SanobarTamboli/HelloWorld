da98ed9f4336b3049a4b654fd3e78b21df1b7a1a
SanobarTamboli <115066018+SanobarTamboli@users.noreply.github.com>
2026-01-28 11:09:12 +0530

    Fix for loop to correctly print numbers

--- HelloWorldProject/src/Sample.java
+++ HelloWorldProject/src/Sample.java
@@ -1,6 +1,7 @@
 public class Sample{
   public static void main (String [] args) {
-    for (int i = 0; i <= 9; i ++);
-    System.out.println("Numbers i=" +i);
+    for (int i = 0; i <= 9; i ++) {
+      System.out.println("Numbers i=" + i);
+    }
   }
 }
