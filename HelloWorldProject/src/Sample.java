ca0967c52475c31f47ffc88b9a485a2175aef200
SanobarTamboli <115066018+SanobarTamboli@users.noreply.github.com>
2026-01-28 11:06:59 +0530

    Rename Main class to Sample and fix for loop syntax

--- HelloWorldProject/src/Sample.java
+++ HelloWorldProject/src/Sample.java
@@ -1,6 +1,6 @@
-public class Main{
+public class Sample{
   public static void main (String [] args) {
-    for (int i = 0, i <= 9, i ++);
+    for (int i = 0; i <= 9; i ++);
     System.out.println("Numbers i=" +i);
   }
 }
